9cf3d349097e4de2a5908c51d034f035610d18e1
LeoCesF <leo.cesfra.goso@gmail.com>
2025-05-27 09:07:38 +0200

    Ejercicio 24 - 27/07/2025 - 9:07

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,29 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> colaQueue = new LinkedList<>();
+        colaQueue.offer("Smith");
+        colaQueue.offer("Montessori");
+        colaQueue.offer("Peralta");
+        colaQueue.offer("House");
 
+        return colaQueue;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
+        for (String item : queue) {
+            System.out.println(item);
+        }
 
+        queue.poll();
+        queue.poll();
+        queue.poll();
+        queue.poll();
     }
 
     public static void main(String[] args) {
-
+        Queue<String> newQueue = createQueue();
+        printAndEmptyQueue(newQueue);
+        System.out.println("==================");
+        printAndEmptyQueue(newQueue);
     }
-
 }
